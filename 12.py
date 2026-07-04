0045dd2c896f66ba312c28aa5361c7dba7abd5b0
yashyadav7326 <yashyadav7326@gmail.com>
2026-07-04 18:35:10 +0530

    update

--- 12.py
+++ 12.py
@@ -1,2 +1,7 @@
-price = float(input("price"))
-print(price)
+word = input("Enter a word: ")
+reversed_word = word[::-1]
+
+if word == reversed_word:
+    print("It is a palindrome")
+else:
+    print("It is not a palindrome")
