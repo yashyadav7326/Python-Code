c0083393fc176e3716baf40db980ea03bf09f717
yashyadav7326 <yashyadav7326@gmail.com>
2026-07-05 11:34:42 +0530

    update

---
+++ 18.py
@@ -0,0 +1,10 @@
+age = int(input("enter a no"))
+
+if age >= 18:
+ print("they are adult")
+ print("they can vote")
+elif age>= 13:
+ print("tennager12")
+ print("they cannot vote ")
+else:
+ print("they are child")  
