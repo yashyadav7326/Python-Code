7a75f3e1dbef66d2568cab7749c6d1e55c89878d
yashyadav7326 <yashyadav7326@gmail.com>
2026-07-05 18:12:11 +0530

    update~

---
+++ 19.py
@@ -0,0 +1,9 @@
+light = input("light colour : ")
+if (light == "red"):
+ print("stop")
+elif (light == "green"):
+ print("go")
+elif (light == "yellow"):
+ print("look")
+else:
+ print("light is broken") 
---
+++ 20.py
@@ -0,0 +1,14 @@
+marks = int(input("enter no : "))
+
+if (marks>=90):
+    print("A")
+elif(marks>=80 and marks<90):
+    print("B")
+elif(marks>=70 and marks<80):
+    print("C")    
+else:
+    print("D")
+
+
+
+
