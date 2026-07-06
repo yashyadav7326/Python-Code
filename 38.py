b943e03986a8e3c326e43766f855b2f7d4509cd1
yashyadav7326 <yashyadav7326@gmail.com>
2026-07-06 18:37:52 +0530

    update~

---
+++ 38.py
@@ -0,0 +1,7 @@
+name = input("enter your name :")
+age = int(input(" enter age : "))
+marks = int(input("enter marks: "))
+
+print("welcome : ", name)
+print("age is :",age)
+print("marks is :",marks)
