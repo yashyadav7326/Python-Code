adee70413999e6463c4cf941887ed24713de6ebc
yashyadav7326 <yashyadav7326@gmail.com>
2026-07-14 18:49:12 +0530

    update

---
+++ str51.py
@@ -0,0 +1,8 @@
+#silicing
+# negative index -1 etc etc  
+#   #Start = -4 → e se start karo.
+#End = -2 → -2 (l) tak jao, lekin usko include mat karo.
+#Python slicing ka rule hai:
+#Start include hota hai, End include nahi hota.
+str = "hello"
+print (str[-4:-2])    
