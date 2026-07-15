de1e9b021955d1c9cfed4595fe8da3f0c9c4047d
yashyadav7326 <yashyadav7326@gmail.com>
2026-07-15 09:50:31 +0530

    update~

---
+++ list65.py
@@ -0,0 +1,13 @@
+#list and tube
+#wahi if ifelse and if
+
+marks1 = 32.2
+marks2 = 34.2
+marks3 = 98.0
+marks4 = 24.5
+marks5 = 84.0
+marks = (32.2 , 34.2 , 98.0 , 24.5 , 84.0)
+
+print(len(marks))
+print(marks[1])
+print(marks[3])
