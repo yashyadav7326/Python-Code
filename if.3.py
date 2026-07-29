9303cdbf6f5808cdbc83a23b96c26167a103a59f
yashyadav7326 <yashyadav7326@gmail.com>
2026-07-29 18:59:38 +0530

    update

---
+++ if.3.py
@@ -0,0 +1,10 @@
+marks = int(input("grade is :"))
+if (marks >= 90):
+ print("A")
+elif (marks >= 80 and marks < 90):
+  print("B")
+elif (marks >=20 and marks <80):
+  print("C")
+else :
+  print("fail")  
+
