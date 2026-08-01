97ca36f99babfa9b432dd6d0d347e975ac578f02
yashyadav7326 <yashyadav7326@gmail.com>
2026-08-01 22:25:58 +0530

    update

---
+++ if.5.py
@@ -0,0 +1,9 @@
+marks = int(input("marks"))
+if (marks>=80 and marks<=100):
+    print("A")
+elif(marks>=40 and marks<80):
+    print("B")
+elif(marks>=20 and marks<40):
+     print("c")  
+else:
+    print("fail")     
