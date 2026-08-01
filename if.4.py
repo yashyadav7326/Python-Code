ba9e67dd32ed223ee1caaeeb70ca1b70d16dbf5b
yashyadav7326 <yashyadav7326@gmail.com>
2026-08-01 18:41:17 +0530

    update

---
+++ if.4.py
@@ -0,0 +1,10 @@
+age = int(input("age : "))
+if (age>=70 and age<=100):
+    print("old")
+elif(age>=18):
+    print("adult")
+elif (age>=10 and age<18 ):
+    print("tennager")
+
+else :
+    print("child")            
