757e8daa8e3f19ab2bc5aa0e5c9d44d208db602e
yashyadav7326 <yashyadav7326@gmail.com>
2026-08-02 22:19:56 +0530

    update

---
+++ if.11.py
@@ -0,0 +1,10 @@
+colour = (input("colour is : "))
+if (colour=="red"):
+    print("danger")
+elif (colour=="green"):
+    print("natural")
+elif(colour=="blue"):
+    print("sky")
+else:
+    print("not a colour")    
+
