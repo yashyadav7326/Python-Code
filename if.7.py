cd3da6a063b72aeaf8dfbc3ebe480b8ad156ee34
yashyadav7326 <yashyadav7326@gmail.com>
2026-08-02 12:35:42 +0530

    update

---
+++ if.7.py
@@ -0,0 +1,9 @@
+veg = (input("enter a veg : "))
+if (veg=="apple"):
+    print("this is veg")
+elif(veg=="banana"):
+    print("this is veg")
+elif(veg=="chikken"):
+    print("not veg but tasty")
+else:
+    print("no eat")           
