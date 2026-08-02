f207418a2ee1a93548fe4553c0cdec6c3570fcd1
yashyadav7326 <yashyadav7326@gmail.com>
2026-08-02 11:57:02 +0530

    update

---
+++ if.6.py
@@ -0,0 +1,11 @@
+food = (input("enter a food : "))
+if (food=="pizza"):
+    print("fast food")
+elif(food=="burger"):
+    print("junk food")
+elif(food=="apple"):
+    print("healthy")
+else:
+    print("not food")        
+
+ 
