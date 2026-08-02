ecc4712ce91244ada76b6a7ce367fc8cac492fc0
yashyadav7326 <yashyadav7326@gmail.com>
2026-08-02 22:14:18 +0530

    update

---
+++ if.10.py
@@ -0,0 +1,11 @@
+animal = (input("animal is : "))
+if (animal=="dog"):
+    print("pet")
+elif (animal=="tiger"):
+    print("danger")
+elif(animal=="cat"):
+    print("pet")
+elif(animal=="lion"):
+    print("danger")
+else:
+    print("not animal")    
